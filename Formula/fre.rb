340e79317f760fbeab3aa6248d80da696f82f54b
Camden Cheek <camden@ccheek.com>
2020-12-08 10:43:31 -0500

    Bump fre version

--- Formula/fre.rb
+++ Formula/fre.rb
@@ -1,8 +1,8 @@
 class Fre < Formula
   desc "A commandline frecency tracker"
   homepage "https://github.com/camdencheek/fre"
-  url "https://github.com/camdencheek/fre/archive/0.3.0.tar.gz"
-  sha256 "098aed8b4f3a451092a605e8fa21ceca086605f0e7cf0009b494b3b217c74696"
+  url "https://github.com/camdencheek/fre/archive/0.3.1.tar.gz"
+  sha256 "22d8dddd1c634031e731ab3966d07aa01acc184b773ae1d9aa5fe5aa6c0c3858"
   license "MIT"
   head "https://github.com/camdencheek/fre.git"
 
